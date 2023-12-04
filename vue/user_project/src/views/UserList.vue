e733a30b3b6b5066eb0a93be0ba9a88276838d13
hyunaahh <laura3014@naver.com>
2023-12-04 09:41:58 +0900

    1204 09:41

--- vue/user_project/src/views/UserList.vue
+++ vue/user_project/src/views/UserList.vue
@@ -1,61 +1,76 @@
 <template>
-  <div class="container">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>No.</th>
-          <th>ID</th>
-          <th>이름</th>
-          <th>성별</th>
-          <th>가입날짜</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr :key="idx" v-for="(user, idx) in userList">
-          <td>{{ user.user_no }}</td>
-          <td>{{ user.user_id }}</td>
-          <td>{{ user.user_name }}</td>
-          <td>{{ user.user_gender }}</td>
-          <td>{{ user.join_date }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class = "container">
+        <table class = "table">
+            <thead>
+                <tr>
+                    <th> No. </th>
+                    <th> ID </th>
+                    <th> 이름 </th>
+                    <th> 성별 </th>
+                    <th> 가입 날짜 </th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr :key="idx" v-for="(user, idx) in userList">
+                    <td>{{user.user_no}} </td>
+                    <td>{{user.user_id}} </td>
+                    <td>{{user.user_name}} </td>
+                    <td>{{user.user_gender}} </td>
+                    <td>{{user.join_date}} </td>
+
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 </template>
 
+
 <script>
-import axios from "axios"; //모듈 불러옴
+    import axios from 'axios';
 
-export default {
-  data() {
-    return {
-      userList: [],
-    };
-  },
+    export default {
+        data(){
+            return{
+                userList : [] // list니까 배열로!  기본타입 구분하기! - 초기값 타입 알려주기! 
+            }
+        },
+        
+        computed : {
+            //computed에서 관련 속성 이렇게 처리하기도 함. 그냥 배열길이 이렇게 wrapping 한거임.
+            count(){
+                return this.userList.length; 
+            }
+        },
 
-  computed: {
-    count() {
-      return this.userList.length; //배열의길이
-    },
-  },
+        //component가 생성되면서 dom으로 mount 되기 전에 created 기반으로 ajax를 돌려야함
+        created(){
+            //해당 component가 처음부터 데이터를 들고있어야하는 순간 created를 쓴다! => 컴포넌트가 초기화할 data관련 진행!  
+            this.getUserList(); // this가 붙어야지 내부에서 검색한다고?
+        },
+        
+        methods : { 
+            //통신을 일으키기! -비동기통신이라 async  await 쓰럭임
+            async getUserList(){
+                let list = (await axios.get('/api/users') //module 가진 애가 axios => 통신의 결과를 객체로 받아옴 **vue.coonfig.js에서 proxy설정 보기1 
+                                    .catch(err => {
+                                        console.log(err);
+                                    })).data; //axios가 실행한 결과는 객체고! > 그 객체안에서의  "data값"만 필요하니까! 
+                
+                //let result = await axios.get('http://localhost:3000/users')
+               // .catch(err => {
+                //     console.log(err);
+                //     })
+                //let list = result.data;
 
-  created() {
-    //컴포넌트가 초기화할 데이터 관련 진행  //마운트는 나중에..?
-    this.getUserList(); //this가 붙어야함
-  },
+                this.userList = list;
+            }
+        }
+    }
 
-  methods: {
-    //디비 데이터 불러오는 함수
-    //axios의 결과는 객체로 가지고옴 {data : {} , status : 200} 우리는 data만 가지고올꺼니까
-    async getUserList() {
-      let result = await axios
-        .get("/user") //const target = "http://localhost:3000";라고 지정해줌
-        .catch((err) => {
-          console.log(err);
-        });
-      let list = result.data;
-      this.userList = list;
-    },
-  },
-};
 </script>
--- vue/user_project/vue.config.js
+++ vue/user_project/vue.config.js
@@ -4,16 +4,18 @@ const target = 'http://localhost:3000'
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  //개발하는동안 적용되는 server
+  // 개발하는동안 적용되는 server → 완성되면 이 설정 빠짐 ! 
   devServer :{
     port : 8080,
 
     //어떤경우에 origin을 대체할건지 정의하는거.. 통신시도할때 사용하는 주소
     proxy : {
       //앞부분이 /로 시작되는 경우 proxy를 시작하겠단 의미.
-      '^/' : { 
-        target, //우리가 가져야하는 origin. target속성의 origin으로 대체하겟다.
-        changeOrigin : true //true로 해줘야 바껴짐 기본적으로 false임.
+      '^/api' : {  //우리가 통신을 일으키는 경로. 슬러시(/)기준으로 넘기면 
+        target, // 우리가 가져야하는 origin. target속성의 origin으로 "대체"하겟다.
+        changeOrigin : true, //true로 해줘야 바껴짐 기본적으로 false임.
+        ws : false, // web socket : 별도 설정없이 웹소켓 ㅈ ㅣ원. 프록시할때 ws 쓸건지 말건지 결정하면 됨. ex) 채팅, 실시간지도업뎃, 
+        pathRewrite : {'^/api' : '/'} // 경로다시쓰기! → api/로 시작하는 애를 : '/'로 쓰겠따(접근하겠다)! 
       }
     }
   }
